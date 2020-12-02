38781daef64b4ffe7133770a43fa12a43992e251
신한 <tlsgks8668@naver.com>
2020-12-02 17:31:41 +0900

    알고리즘 시뮬레이션

--- BaekJoon/src/simulation/HumanMigration_16234.java
+++ BaekJoon/src/simulation/HumanMigration_16234.java
@@ -1,6 +1,15 @@
 package simulation;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 /*
+
+https://www.acmicpc.net/problem/16234
 
 인구 이동 분류
 시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
@@ -93,6 +102,126 @@ L = 20, R = 50이기 때문에, 아래와 같이 국경선이 열린다.
 
 */
 
-public class HumanMigration_16234 {
 
+// 메모리 : 294284 KB
+// 시간 : 648 ms
+// 약 2시간 가량 소요
+// 다르게 풀어보기
+public class HumanMigration_16234 {
+	private static int N, L, R;
+	private static int[][] map;
+	private static boolean[][] visited;
+	private static List<Pair> borderList;
+	
+	private static int dx[] = {1, -1, 0, 0};
+	private static int dy[] = {0, 0, 1, -1};
+	
+	static class Pair {
+		int x, y;
+		public Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][N];
+		int count = 0;
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		while(true) {
+			boolean flag = false;
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(isFunction(i, j)) {
+						flag = true;
+					}
+				}
+			}
+			
+			if(!flag) break;
+			
+			visited = new boolean[N][N];
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(!visited[i][j]) {
+						borderList = new ArrayList<>();
+						borderList.add(new Pair(i, j));
+						migration(i, j);
+						populationCount();
+					}
+				}
+			
+			}
+			count++;
+		}
+		
+		System.out.println(count);
+		
+		br.close();
+	}
+	
+	private static boolean isFunction(int x, int y) {
+		for(int i = 0; i < 4; i++) {
+			int X = x + dx[i];
+			int Y = y + dy[i];
+			
+			if(X >= 0 && X < N && Y >= 0 && Y < N) {
+				if(Math.abs(map[x][y]-map[X][Y]) >= L && Math.abs(map[x][y]-map[X][Y]) <= R) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	private static void populationCount() {
+		int sum = 0;
+		int result = 0;
+		for(Pair p : borderList) {
+			sum += map[p.x][p.y];
+		}
+		if(borderList.size() > 0) {
+			result = sum/borderList.size();			
+			for(Pair p : borderList) {
+				map[p.x][p.y] = result; 
+			}
+		}
+		
+		
+	}
+	
+	private static void migration(int x, int y) {
+		if(!visited[x][y]) {
+			visited[x][y] = true;
+		}
+		
+		for(int i = 0; i < 4; i++) {
+			int X = x + dx[i];
+			int Y = y + dy[i];
+			
+			if(X >= 0 && X < N && Y >= 0 && Y < N && !visited[X][Y]) {
+				if(Math.abs(map[x][y]-map[X][Y]) >= L && Math.abs(map[x][y]-map[X][Y]) <= R) {
+					visited[X][Y] = true;
+					borderList.add(new Pair(X, Y));
+					migration(X, Y);
+				}
+			}
+		}
+		
+	}
+	
 }
