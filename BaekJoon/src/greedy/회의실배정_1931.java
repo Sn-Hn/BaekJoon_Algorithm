ec0ae828e15aeac0291619e095f45732cc3902f2
ShinHan <tlsgks8668@naver.com>
2021-06-21 20:56:52 +0900

    그리디

---
+++ BaekJoon/src/greedy/회의실배정_1931.java
@@ -0,0 +1,105 @@
+package greedy;
+
+/*
+
+회의실 배정
+시간 제한   메모리 제한   제출   정답   맞은 사람   정답 비율
+2 초   128 MB   81762   24189   17421   28.757%
+문제
+한 개의 회의실이 있는데 이를 사용하고자 하는 N개의 회의에 대하여 회의실 사용표를 만들려고 한다.
+각 회의 I에 대해 시작시간과 끝나는 시간이 주어져 있고, 각 회의가 겹치지 않게 하면서 회의실을 사용할 수 있는 회의의 최대 개수를 찾아보자.
+단, 회의는 한번 시작하면 중간에 중단될 수 없으며 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다.
+회의의 시작시간과 끝나는 시간이 같을 수도 있다.
+이 경우에는 시작하자마자 끝나는 것으로 생각하면 된다.
+
+입력
+첫째 줄에 회의의 수 N(1 ≤ N ≤ 100,000)이 주어진다.
+둘째 줄부터 N+1 줄까지 각 회의의 정보가 주어지는데 이것은 공백을 사이에 두고 회의의 시작시간과 끝나는 시간이 주어진다.
+시작 시간과 끝나는 시간은 231-1보다 작거나 같은 자연수 또는 0이다.
+
+출력
+첫째 줄에 최대 사용할 수 있는 회의의 최대 개수를 출력한다.
+
+예제 입력 1
+11
+1 4
+3 5
+0 6
+5 7
+3 8
+5 9
+6 10
+8 11
+8 12
+2 13
+12 14
+예제 출력 1
+4
+힌트
+(1,4), (5,7), (8,11), (12,14) 를 이용할 수 있다.
+
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/1931
+public class 회의실배정_1931 {
+    private static int N;
+    private static Meeting[] meetings;
+
+    private static class Meeting implements Comparable<Meeting> {
+        int start;
+        int end;
+
+        public Meeting(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Meeting o) {
+            if (end == o.end) {
+                return start - o.start;
+            }
+
+            return end - o.end;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        meetings = new Meeting[N];
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            meetings[i] = new Meeting(start, end);
+        }
+
+        Arrays.sort(meetings);
+
+        System.out.println(getMaxUsedMeetingCount());
+
+        br.close();
+    }
+
+    private static int getMaxUsedMeetingCount() {
+        int currentEnd = meetings[0].end;
+        int count = 1;
+        for (int i = 1; i < N; i++) {
+            if (currentEnd <= meetings[i].start) {
+                currentEnd = meetings[i].end;
+                count ++;
+            }
+        }
+
+        return count;
+    }
+}
