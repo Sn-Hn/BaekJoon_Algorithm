af53e93b58842cdadcfff640307ec4c341eb0290
ShinHan <tlsgks8668@naver.com>
2021-10-22 02:06:50 +0900

    2

---
+++ BaekJoon/src/누적합/구간합구하기5_리팩토링.java
@@ -0,0 +1,48 @@
+package 누적합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 구간합구하기5_리팩토링 {
+    private static int N;
+    private static int M;
+    private static int[][] prefixSum;
+
+    private static int sum(int x1, int y1, int x2, int y2) {
+        return prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        prefixSum = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            result.append(sum(x1, y1, x2, y2))
+                    .append("\n");
+        }
+
+        System.out.println(result.toString().trim());
+
+        br.close();
+    }
+}
